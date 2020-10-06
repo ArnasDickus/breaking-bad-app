1a31e03375e3a8c6433c12122ba8ce8a2dabde72
Arnas Dickus <arnasdickus@protonmail.com>
2020-10-06 07:15:06 +0300

    finished styling footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
